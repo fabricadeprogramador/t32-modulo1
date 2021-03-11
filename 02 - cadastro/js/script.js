f200d93d749a9eb3b426e5218056a771ee206a4f
Vinicius Souza <viniciuspodi@gmail.com>
2021-03-10 20:26:32 -0400

    Adicionando validação no cadastro de clientes

--- 02 - cadastro/js/script.js
+++ 02 - cadastro/js/script.js
@@ -7,9 +7,58 @@ function fecharMensagens() {
 }
 
 function validar() {
-  var nome = document.getElementById('nome').value
+  //Variável que vai acumular as mensagens de erro
+  var bufferMensagem = ''
 
+  //Captura dos valores
+  var nome = document.getElementById('nome').value
+  var email = document.getElementById('email').value
+  var sexoMasculino = document.getElementById('masc').checked
+  var sexoFeminino = document.getElementById('fem').checked
+  var qtdCursosSelecionados = document.querySelectorAll(
+    '[type=checkbox]:checked'
+  ).length
+  var opcaoSelectEstados = document.getElementById('estado').value
+  var foto = document.getElementById('foto').value
+  var dataNasc = document.getElementById('datanasc').value
+
+  //Validações
   if (nome == '') {
-    console.log('Faltando preencher o nome')
+    bufferMensagem = bufferMensagem + '\nO campo nome é obrigatório'
+  }
+
+  if (email == '') {
+    bufferMensagem = bufferMensagem + '\nO campo e-mail é obrigatório'
+  }
+
+  if (!sexoMasculino && !sexoFeminino) {
+    bufferMensagem = bufferMensagem + '\nPreencha o campo sexo'
+  }
+
+  if (qtdCursosSelecionados == 0) {
+    bufferMensagem =
+      bufferMensagem + '\nSelecione pelo menos um curso de interesse'
+  }
+
+  if (opcaoSelectEstados == '') {
+    bufferMensagem = bufferMensagem + '\nSelecione o estado'
+  }
+
+  if (foto == '') {
+    bufferMensagem = bufferMensagem + '\nSelecione uma foto'
+  }
+
+  if (dataNasc == '') {
+    bufferMensagem =
+      bufferMensagem + '\nO campo data de nascimento é obrigatório'
+  }
+
+  if (bufferMensagem == '') {
+    document.getElementById('textoMensagem').textContent =
+      'Cliente salvo com sucesso!'
+    document.getElementById('div-msg').classList.add('show')
+  } else {
+    document.getElementById('textoMensagem').textContent = bufferMensagem
+    document.getElementById('div-msg').classList.add('show')
   }
 }
