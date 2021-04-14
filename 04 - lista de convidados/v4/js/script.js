9562346e5850ab719addb008fd5f82280f917384
Vinicius Souza <viniciuspodi@gmail.com>
2021-04-14 19:58:33 -0400

    Iniciando correção da lista de convidados v4

--- 04 - lista de convidados/v4/js/script.js
+++ 04 - lista de convidados/v4/js/script.js
@@ -3,63 +3,107 @@ class ListaConvidadoController {
     this.contador = 1
   }
 
+  lerDados() {
+    let convidado = {}
+
+    convidado.nome = document.getElementById('nomeConvidado').value
+    convidado.idade = document.getElementById('idadeConvidado').value
+    convidado.sexo = document.getElementById('sexoConvidado').value
+
+    return convidado
+  }
+
   adicionar() {
-    let nomeConvidado = document.getElementById('nomeConvidado').value
+    let convidadoNovo = this.lerDados()
 
-    if (this.validar(nomeConvidado)) {
-      let lista = document.getElementById('lista')
+    if (this.validar(convidadoNovo)) {
+      //Busca o elemento tabela
+      let tabela = document.getElementById('tabelaCorpo')
 
-      let itemNovo = document.createElement('div')
-      itemNovo.id = 'item-' + this.contador
+      //Insere uma nova linha na tabela
+      let linha = tabela.insertRow()
 
-      let divNumero = document.createElement('div')
-      let divNome = document.createElement('div')
-      let divRemover = document.createElement('div')
+      //Setando ID da linha
+      linha.id = 'linha-' + this.contador
 
-      divNumero.innerText = this.contador
-      divNome.innerText = nomeConvidado
+      //Insere as colunas na linha
+      let colunaId = linha.insertCell()
+      let colunaNome = linha.insertCell()
+      let colunaIdade = linha.insertCell()
+      let colunaSexo = linha.insertCell()
+      let colunaRemover = linha.insertCell()
+      let colunaEditar = linha.insertCell()
 
+      //Setando IDs das colunas necessárias
+      colunaNome.id = 'colunaNome-' + this.contador
+      colunaIdade.id = 'colunaIdade-' + this.contador
+      colunaSexo.id = 'colunaSexo-' + this.contador
+
+      //Seta os valores nas colunas
+      colunaId.innerText = this.contador
+      colunaNome.innerText = convidadoNovo.nome
+      colunaIdade.innerText = convidadoNovo.idade
+      colunaSexo.innerText = convidadoNovo.sexo
+
+      //Cria os elementos de imagem
       let imagemRemover = document.createElement('img')
+      let imagemEditar = document.createElement('img')
 
+      //Setar as propriedades das imagens
       imagemRemover.src = 'img/delete.svg'
+      imagemEditar.src = 'img/edit.svg'
+
       imagemRemover.setAttribute(
         'onclick',
         'convidadoController.remover(' + this.contador + ')'
       )
+      imagemEditar.setAttribute(
+        'onclick',
+        'convidadoController.editar(' + this.contador + ')'
+      )
 
-      divRemover.appendChild(imagemRemover)
-      itemNovo.appendChild(divNumero)
-      itemNovo.appendChild(divNome)
-      itemNovo.appendChild(divRemover)
-      lista.appendChild(itemNovo)
+      //Adicionando as imagens como filhas das celulas referentes
+      colunaRemover.appendChild(imagemRemover)
+      colunaEditar.appendChild(imagemEditar)
 
+      //Incrementa o contador
       this.contador++
 
       this.cancelar()
-    } else {
-      alert('Digite o nome do convidado!')
     }
   }
 
-  remover(itemId) {
-    if (
-      confirm(
-        'Tem certeza que deseja remover o elemento numero ' + itemId + '?'
-      )
-    ) {
-      document.getElementById('item-' + itemId).remove()
-    }
-  }
+  remover(itemId) {}
 
-  validar(nome) {
-    if (nome == '') {
+  editar(itemId) {}
+
+  validar(convidado) {
+    let mensagemValidacao = ''
+
+    if (convidado.nome == '') {
+      mensagemValidacao += 'Campo nome é obrigatório! \n'
+    }
+
+    if (convidado.idade == '') {
+      mensagemValidacao += 'Campo idade é obrigatório! \n'
+    }
+
+    if (convidado.sexo == '') {
+      mensagemValidacao += 'Campo sexo é obrigatório! \n'
+    }
+
+    if (mensagemValidacao != '') {
+      alert(mensagemValidacao)
       return false
     }
+
     return true
   }
 
   cancelar() {
     document.getElementById('nomeConvidado').value = ''
+    document.getElementById('idadeConvidado').value = ''
+    document.getElementById('sexoConvidado').value = ''
   }
 }
 
