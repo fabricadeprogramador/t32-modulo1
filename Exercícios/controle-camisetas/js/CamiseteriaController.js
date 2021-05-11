cb8efe9b079214b510087bffd70376b9e9154102
Vinicius Souza <viniciuspodi@gmail.com>
2021-05-10 20:01:50 -0400

    Finalizando exercício da camiseteria

--- Exercícios/controle-camisetas/js/CamiseteriaController.js
+++ Exercícios/controle-camisetas/js/CamiseteriaController.js
@@ -1,6 +1,8 @@
 class CamiseteriaController {
   constructor() {
     this.pedidos = []
+    this.geradorId = 1
+    this.pedidoDeEdicao = null
   }
 
   criarPedido() {
@@ -31,17 +33,34 @@ class CamiseteriaController {
     return false
   }
 
+  calcularTotal(pedido) {
+    return pedido.qtdP * 10 + pedido.qtdM * 12 + pedido.qtdG * 15
+  }
+
   salvar() {
     let pedidoNovo = this.criarPedido()
 
     if (this.validar(pedidoNovo)) {
-      pedidoNovo.total =
-        pedidoNovo.qtdP * 10 + pedidoNovo.qtdM * 12 + pedidoNovo.qtdG * 15
+      if (this.pedidoDeEdicao == null) {
+        pedidoNovo.id = this.geradorId
+        pedidoNovo.total = this.calcularTotal(pedidoNovo)
 
-      this.pedidos.push(pedidoNovo)
-      this.gerarTabela()
-      this.limpar()
+        this.pedidos.push(pedidoNovo)
+        this.geradorId++
+      } else {
+        for (let i = 0; i < this.pedidos.length; i++) {
+          if (this.pedidos[i].id == this.pedidoDeEdicao) {
+            this.pedidos[i].qtdP = pedidoNovo.qtdP
+            this.pedidos[i].qtdM = pedidoNovo.qtdM
+            this.pedidos[i].qtdG = pedidoNovo.qtdG
+            this.pedidos[i].total = this.calcularTotal(pedidoNovo)
+          }
+        }
+      }
     }
+
+    this.gerarTabela()
+    this.limpar()
   }
 
   gerarTabela() {
@@ -75,8 +94,17 @@ class CamiseteriaController {
     imagemRemover.src = 'img/delete.svg'
     imagemEditar.src = 'img/edit.svg'
 
-    //TODO: SETAR OS ONCLICKS
+    imagemRemover.setAttribute(
+      'onclick',
+      'camiseteriaController.remover(' + pedido.id + ')'
+    )
 
+    imagemEditar.setAttribute(
+      'onclick',
+      `camiseteriaController.editar(${JSON.stringify(pedido)})`
+    )
+
+    // `camiseteriaController.editar(${pedido.id}, ${pedido.qtdP}, ${pedido.qtdM}, ${pedido.qtdG})`
     colunaRemover.appendChild(imagemRemover)
     colunaEditar.appendChild(imagemEditar)
   }
@@ -85,6 +113,28 @@ class CamiseteriaController {
     document.getElementById('qtdP').value = ''
     document.getElementById('qtdM').value = ''
     document.getElementById('qtdG').value = ''
+
+    this.pedidoDeEdicao = null
+  }
+
+  remover(idPedido) {
+    if (confirm('Tem certeza que deseja remover este pedido?')) {
+      for (let i = 0; i < this.pedidos.length; i++) {
+        if (idPedido == this.pedidos[i].id) {
+          this.pedidos.splice(i, 1)
+          break
+        }
+      }
+
+      this.gerarTabela()
+    }
+  }
+
+  editar(pedido) {
+    this.pedidoDeEdicao = pedido.id
+    document.getElementById('qtdP').value = pedido.qtdP
+    document.getElementById('qtdM').value = pedido.qtdM
+    document.getElementById('qtdG').value = pedido.qtdG
   }
 }
 
