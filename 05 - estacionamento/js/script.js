ad1aac0b58271af143d894c1c6ff190c63c59417
Vinicius Souza <viniciuspodi@gmail.com>
2021-05-17 20:05:05 -0400

    Iniciando correção do exercício do estacionamento

--- 05 - estacionamento/js/script.js
+++ 05 - estacionamento/js/script.js
@@ -2,6 +2,7 @@ class EstacionamentoController {
   constructor() {
     this.geradorId = 1
     this.carros = []
+    this.editId = -1
   }
 
   salvar() {
@@ -23,6 +24,24 @@ class EstacionamentoController {
 
     this.carros.push(carroNovo)
     this.geradorId++
+  }
+
+  excluir(id) {
+    if (confirm("Deeja realmente deletar o ID " + id)) {
+      let i = 0
+      let indice = -1
+
+      while (i < this.carros.length && indice == -1) {
+        if (this.carros[i].id == id) {
+          indice = i
+          this.carros.splice(i, 1)
+        }
+
+        i++
+      }
+
+      this.gerarTabela()
+    }
   }
 
   lerDados() {
@@ -50,7 +69,57 @@ class EstacionamentoController {
     document.getElementById("anoVeiculo").value = ""
   }
 
-  gerarTabela() {}
+  gerarTabela() {
+    document.getElementById("tbody").innerHTML = ""
+    for (let i = 0; i < this.carros.length; i++) {
+      this.inserirLinha(this.carros[i])
+    }
+  }
+
+  inserirLinha(carro) {
+    let tabela = document.getElementById("tbody")
+
+    let linha = tabela.insertRow()
+    // linha.insertCell().innerText = carro.nome
+
+    let colunaNome = linha.insertCell()
+    let colunaMarca = linha.insertCell()
+    let colunaAno = linha.insertCell()
+    let colunaEditar = linha.insertCell()
+    let colunaExcluir = linha.insertCell()
+
+    colunaNome.innerText = carro.nome
+    colunaMarca.innerText = carro.marca
+    colunaAno.innerText = carro.ano
+
+    let carroParametro = JSON.stringify(carro)
+
+    colunaEditar.innerHTML = `<img src="img/edit.svg" onclick="estacionamentoController.preparaEdicao(${carroParametro})">`
+    colunaExcluir.innerHTML = `<img src="img/delete.svg" onclick="estacionamentoController.excluir(${carro.id})">`
+
+    //O CÓDICO COMENTADO É EQUIVALENTE AS DUAS LINHAS ANTERIORES
+
+    // let imgEditar = document.createElement("img")
+    // let imgExcluir = document.createElement("img")
+
+    // imgEditar.src = "img/edit.svg"
+    // imgExcluir.src = "img/delete.svg"
+
+    // imgEditar.setAttribute("onclick", `estacionamentoController.editar(${carro.id})`)
+    // imgExcluir.setAttribute("onclick", `estacionamentoController.excluir(${carro.id})`)
+
+    // colunaEditar.appendChild(imgEditar)
+    // colunaExcluir.appendChild(imgExcluir)
+  }
+
+  preparaEdicao(carro) {
+    alert("aaaa")
+    // this.editId = carro.id;
+
+    // document.getElementById("nomeVeiculo").value = carro.nome;
+    // document.getElementById("marcaVeiculo").value = carro.marca;
+    // document.getElementById("anoVeiculo").value = carro.ano;
+  }
 
   calcularResultado() {}
 }
